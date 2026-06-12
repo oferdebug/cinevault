9379079aed9a53896a1436af90ba5cf3e4838a21
oferdebug <ofercohen404@gmail.com>
2026-06-12 20:15:30 +0300

    wip: Layout.jsx before reinstall

--- apps/web/src/components/Layout.jsx
+++ apps/web/src/components/Layout.jsx
@@ -75,7 +75,7 @@ const IconUser = () => (
 		strokeLinejoin="round"
 		aria-hidden="true"
 	>
-		<path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2" />
+		<path d="M20 21v-2a4 4 0 0 0-rm -f ~/.config/monitors.xml4-4H8a4 4 0 0 0-4 4v2" />
 		<circle cx="12" cy="7" r="4" />
 	</svg>
 );
